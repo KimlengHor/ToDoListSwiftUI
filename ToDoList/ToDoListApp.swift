008c8fa31218b3a743c964b86e61761c734dfc0a
Kimleng <kimlenghor2006@gmail.com>
2023-03-29 18:09:03 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Kimleng Hor on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
